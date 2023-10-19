f0c0a1e45cd7df14fbc37c5e4711f725784fa747
joses145 <josea789@gmail.com>
2023-10-19 21:51:41 +1100

    return temperature for any station and day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,6 +12,9 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
     temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt" #el método zfill me permite rellenar con ceros una cadena de texto
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {
         "station": station,
         "date": date,
@@ -19,4 +23,4 @@ def api(station, date):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port= 5001)
